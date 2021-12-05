006c6b1895567e09eadfd5e518b6e3d64ca19f92
Tyler Southwick <tyler@northfuse.io>
2021-12-05 12:07:57 -0800

    day2 part2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -8,19 +8,42 @@ fn main() -> anyhow::Result<()> {
     part1.r#move(&commands);
     println!("part1: {}", part1.result());
 
+    let mut part2 = PositionPart2::default();
+    part2.r#move(&commands);
+    println!("part2: {}", part2.result());
+
     Ok(())
 }
 
 #[derive(Default, PartialEq, Debug)]
 struct Position {
     horizontal : usize,
-    depth : usize
+    depth : usize,
+}
+
+#[derive(Default, PartialEq, Debug)]
+struct PositionPart2 {
+    horizontal : usize,
+    depth : usize,
+    aim : usize,
 }
 
 impl Position {
     fn r#move(&mut self, commands : &Vec<MovementCommand>) {
         for command in commands.into_iter() {
-            command.r#move(self)
+            command.move_part1(self)
+        }
+    }
+
+    fn result(&self) -> usize {
+        self.horizontal * self.depth
+    }
+}
+
+impl PositionPart2 {
+    fn r#move(&mut self, commands : &Vec<MovementCommand>) {
+        for command in commands.into_iter() {
+            command.move_part2(self)
         }
     }
 
@@ -37,7 +60,7 @@ enum MovementCommand {
 }
 
 impl MovementCommand {
-    fn r#move(&self, position : &mut Position) {
+    fn move_part1(&self, position : &mut Position) {
         match self {
             MovementCommand::Forward(count) => {
                 position.horizontal += count
@@ -47,6 +70,21 @@ impl MovementCommand {
             },
             MovementCommand::Up(count) => {
                 position.depth -= count
+            },
+        }
+    }
+
+    fn move_part2(&self, position : &mut PositionPart2) {
+        match self {
+            MovementCommand::Forward(count) => {
+                position.horizontal += count;
+                position.depth += position.aim * count;
+            },
+            MovementCommand::Down(count) => {
+                position.aim += count;
+            },
+            MovementCommand::Up(count) => {
+                position.aim -= count;
             },
         }
     }
@@ -106,5 +144,23 @@ mod test {
             horizontal: 15,
         });
         assert_eq!(pos.result(), 150);
+    }
+
+    #[test]
+    fn move_commands_part2() {
+        let commands = vec![
+            Forward(5),
+            Down(5),
+            Forward(8),
+            Up(3),
+            Down(8),
+            Forward(2),
+        ];
+        let mut pos = PositionPart2::default();
+        pos.r#move(&commands);
+
+        assert_eq!(pos.depth, 60);
+        assert_eq!(pos.horizontal, 15);
+        assert_eq!(pos.result(), 900);
     }
 }
